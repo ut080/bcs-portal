8700d5a961eb00cbf698bbc4881bf59666441283
Robert Hawk <robert.hawk@utwg.cap.gov>
2023-09-11 15:33:05 -0600

    fix: Conflicting subnet CIDR blocks (#11)

--- deployments/terraform/vpc.tf
+++ deployments/terraform/vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "bcs_portal" {
 
 resource "aws_subnet" "bcs_portal_db_aza" {
   vpc_id = aws_vpc.bcs_portal.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
   availability_zone = "${var.region}a"
 
   tags = {
@@ -18,7 +18,7 @@ resource "aws_subnet" "bcs_portal_db_aza" {
 
 resource "aws_subnet" "bcs_portal_db_azb" {
   vpc_id = aws_vpc.bcs_portal.id
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "${var.region}b"
 
   tags = {
