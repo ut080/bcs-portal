d3cc91cdc8304137896c7ffc9b3885ac5ad4042a
Robert Hawk <robert.hawk@utwg.cap.gov>
2023-09-11 15:30:33 -0600

    fix: Addressed Terraform apply errors (#10)

--- deployments/terraform/vpc.tf
+++ deployments/terraform/vpc.tf
@@ -1,14 +1,25 @@
 resource "aws_vpc" "bcs_portal" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
 
   tags = {
     Service = "bcs-portal"
   }
 }
 
-resource "aws_subnet" "bcs_portal_db" {
+resource "aws_subnet" "bcs_portal_db_aza" {
   vpc_id = aws_vpc.bcs_portal.id
   cidr_block = "10.0.0.0/24"
+  availability_zone = "${var.region}a"
+
+  tags = {
+    Service = "bcs-portal"
+  }
+}
+
+resource "aws_subnet" "bcs_portal_db_azb" {
+  vpc_id = aws_vpc.bcs_portal.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "${var.region}b"
 
   tags = {
     Service = "bcs-portal"
