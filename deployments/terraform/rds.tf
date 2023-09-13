74a349923cb61c2abe4926aa3248894c7557ca6f
Robert Hawk <robert.hawk@gmail.com>
2023-09-13 16:13:11 -0600

    chore: Rebuilding infrastructure
    This probably wasn't one of my better ideas anyway.

--- deployments/terraform/rds.tf
+++ deployments/terraform/rds.tf
@@ -11,7 +11,6 @@ resource "aws_db_instance" "bcs_portal" {
   identifier                    = "bcs-portal-db"
   instance_class                = "db.t3.micro"
   manage_master_user_password   = true
-  master_user_secret_kms_key_id = aws_kms_key.bcs_portal.id
   username                      = "postgres"
 
   tags = {
