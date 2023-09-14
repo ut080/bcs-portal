40acc2e2ab1380eacfd57c2a7407fa411468d6ce
Robert Hawk <robert.hawk@gmail.com>
2023-09-14 11:47:07 -0600

    fix: Expand RDS KMS key policy for TF Cloud

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -3,6 +3,15 @@ variable "region" {
   default     = "us-west-2"
 }
 
+variable "aws_principal_arn" {
+  description = "Value to use as the principal for IAM users for KMS keys"
+  default = ""
+}
+
+variable "terraform_cloud_role_arn" {
+  description = "The ARN to use when granting permissions to the Terraform Cloud role"
+}
+
 variable "amplify_repository" {
   description = "Git repository used with AWS Amplify"
   default = "https://github.com/ut080/bcs-portal"
@@ -10,5 +19,5 @@ variable "amplify_repository" {
 
 variable "bcs_portal_domain" {
   description = "The domain name for the BCS portal"
-  default = "blackhawkcadetsquadron.org"
+  default = ""
 }
